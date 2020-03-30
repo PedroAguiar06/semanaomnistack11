c021f3b281daa8dd671cba6627b64da7abf94125
PedroAguiar06 <pedrooliveiradeaguiar@gmail.com>
2020-03-30 16:31:16 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './global.css';
+
+import Routes from './routes';
+
+function App(){
+    return (
+        <Routes />
+    );
 }
-
 export default App;
+
+/*toda vez que for preciso mudar o valor do contador(counter), eu vou chamar a funcção setCounter
+    const [counter, setCounter] = useState(0);
+    //Quando usamos o metodo useState, ele retorna como uma array, onde 1°pos é o valor e 2° é a func de att... ex: Array[valor, funcDeAtt]
+    function increment(){
+        setCounter(counter + 1);
+    }*/
+   
+        /*<div>
+                 toda vez que for utilizar uma var dentro do html, use chaves {} 
+            <Header>Contador: {counter} </Header>
+            <button onClick={increment}>Incrementar</button>
+        </div>*/
